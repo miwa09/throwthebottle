c21e3101ab9d4f9f4afd68758665238ea9251ee9
Joni Halttunen <joni.halttunen@gmail.com>
2020-01-24 11:25:55 +0200

    maalitriggeri tägillä

---
+++ ThrowTheBottle/Assets/GoalTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "throwable") {
+            Debug.Log("voitit pelin :)");
+        }
+    }
+}
