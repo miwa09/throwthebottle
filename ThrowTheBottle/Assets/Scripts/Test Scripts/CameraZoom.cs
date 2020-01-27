81a2d269ef544dd6bed1541d421d7e745fd2d837
Viljami Perheentupa <pviljo@gmail.com>
2020-01-27 12:41:17 +0200

    no message

---
+++ ThrowTheBottle/Assets/Scripts/Test Scripts/CameraZoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    Vector3 resetForward;
+    public bool active = false;
+    public float minFOV = 15;
+    float resetFOV;
+    GameObject objToFollow;
+    void Start()
+    {
+        resetFOV = Camera.main.fieldOfView;
+        resetForward = transform.forward;
+    }
+
+    void Update()
+    {
+        objToFollow = GameObject.FindGameObjectWithTag("throwable");
+        if (active) {
+            Zoom();
+        } else ResetZoom();
+    }
+
+    void Zoom() {
+        transform.forward = Vector3.MoveTowards(transform.forward, (objToFollow.transform.position - transform.position).normalized, Time.deltaTime * 1.5f);
+        if (Camera.main.fieldOfView > minFOV) {
+            Camera.main.fieldOfView -= Time.deltaTime * 100;
+        }
+        if (Camera.main.fieldOfView < minFOV) {
+            Camera.main.fieldOfView = minFOV;
+        }
+    }
+
+    void ResetZoom() {
+        transform.forward = Vector3.MoveTowards(transform.forward, resetForward, Time.deltaTime * 2);
+        if (Camera.main.fieldOfView < resetFOV) {
+            Camera.main.fieldOfView += Time.deltaTime * 200;
+        }
+        if (Camera.main.fieldOfView > resetFOV) {
+            Camera.main.fieldOfView = resetFOV;
+        }
+    }
+}
---
+++ ThrowTheBottle/Assets/Scripts/Test Scripts/SlowmoObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowmoObject : MonoBehaviour
+{
+    bool active = false;
+    public float maxSlow = 0.5f;
+    public float timeToMax = 1;
+
+    void Update()
+    {
+        if (active) {
+            SlowMo();
+        } else RegularMo();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("throwable")) {
+            active = true;
+            Camera.main.gameObject.GetComponent<CameraZoom>().active = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("throwable")) {
+            active = false;
+            Camera.main.gameObject.GetComponent<CameraZoom>().active = false;
+        }
+    }
+
+    void SlowMo() {
+        if (Time.timeScale > maxSlow) {
+            Time.timeScale -= Time.deltaTime * 5;
+        }
+        if (Time.timeScale < maxSlow) {
+            Time.timeScale = maxSlow;
+        }
+    }
+
+    void RegularMo() {
+        if (Time.timeScale < 1) {
+            Time.timeScale += Time.deltaTime * 2;
+        }
+        if (Time.timeScale > 1) {
+            Time.timeScale = 1;
+        }
+    }
+}
