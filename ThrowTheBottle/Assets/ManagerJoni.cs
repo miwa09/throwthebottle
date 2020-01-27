40eb61bd6a06458f3842513ad3952050f88b6f96
Joni Halttunen <joni.halttunen@gmail.com>
2020-01-27 15:55:41 +0200

    Kentän läpäisyn testailua omassa scenessä

---
+++ ThrowTheBottle/Assets/ManagerJoni.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerJoni : MonoBehaviour
+{
+    public GameObject levelCompleteUI;
+    public void LevelComplete() {
+        levelCompleteUI.SetActive(true);
+        Debug.Log("voitit pelin");
+    }
+}
--- ThrowTheBottle/Assets/Scripts/GoalTrigger.cs
+++ ThrowTheBottle/Assets/Scripts/GoalTrigger.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour
 {
+    ManagerJoni manager;
+    private void Start() {
+        manager = FindObjectOfType<ManagerJoni>();
+    }
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "throwable") {
-            Debug.Log("voitit pelin :)");
+            manager.Invoke("LevelComplete", 1);
         }
     }
 }
