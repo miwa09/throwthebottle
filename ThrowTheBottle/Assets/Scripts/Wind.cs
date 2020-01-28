a563cbb69fc74cbd63802d63ce396861830853b6
Viljami Perheentupa <pviljo@gmail.com>
2020-01-28 15:30:19 +0200

    tuuli

---
+++ ThrowTheBottle/Assets/Scripts/Wind.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    //OHJEET: Area on se alue, missä tuuli vaikuttaa. Se voi olla mikä tahansa collider asetettuna triggeriksi. Direction on se suunta mihin tuuli puskee.
+    //Saadaksesi tuulen vaikuttamaan esineihin, niissä pitää olla joko tagi "throwable" tai "WindAffected". Rigidbodyssä oleva "Mass" vaikuttaa tuulen voimakkuuteen.
+
+    public float speed;
+    public Transform direction;
+    Vector3 pushDirection;
+
+    private void Update() {
+        pushDirection = (direction.position - transform.position).normalized;
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("throwable") || other.CompareTag("WindAffected")) {
+            if (other.GetComponent<Rigidbody>() != null) {
+                other.GetComponent<Rigidbody>().AddForce(pushDirection * speed, ForceMode.Force);
+            }
+        }
+    }
+}
