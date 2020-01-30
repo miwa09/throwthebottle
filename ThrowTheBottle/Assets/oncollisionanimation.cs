9d29f5403f919e51915dcaeecc09321bb7b59bcf
Laura L <lmlehtinen@gmail.com>
2020-01-30 12:01:55 +0200

    äänii ja animaatioo

---
+++ ThrowTheBottle/Assets/oncollisionanimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oncollisionanimation : MonoBehaviour
+{
+
+    public Animation myanimation;
+
+    void OnCollisionEnter()
+    {
+       myanimation.Play();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
