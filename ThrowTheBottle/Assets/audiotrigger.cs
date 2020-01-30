2e5ef9ba8fd1713d4bf7e36113d5ad0b8f83ef29
Laura L <lmlehtinen@gmail.com>
2020-01-30 10:52:34 +0200

    äänihommels

---
+++ ThrowTheBottle/Assets/audiotrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiotrigger : MonoBehaviour
+{
+    public AudioClip soundtoplay;
+    public float Volume;
+    AudioSource sound;
+    public bool AlreadyPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter()
+    {
+
+        sound.PlayOneShot(soundtoplay, Volume);
+
+    }
+}
+// Update is called once per frame
+
