9018dbea70be29da33aea8c725ce0ee7bc59ee2a
Viljami Perheentupa <pviljo@gmail.com>
2020-02-04 10:31:48 +0200

    no message

--- ThrowTheBottle/Assets/Scripts/ModeSelector.cs
+++ ThrowTheBottle/Assets/Scripts/ModeSelector.cs
@@ -7,30 +7,6 @@ public class ModeSelector : MonoBehaviour
 {
     public bool chaos = false;
 
-    private void Awake() {
-        if (PlayerPrefs.GetInt("mistake") == 1) {
-            PlayerPrefs.SetInt("mistake", 2);
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("ViljamiScene");
-            return;
-        }
-        if (PlayerPrefs.GetInt("mistake") == 2) {
-            PlayerPrefs.SetInt("mistake", 3);
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("ViljamiScene");
-            return;
-        }
-        if (PlayerPrefs.GetInt("mistake") == 3) {
-            PlayerPrefs.SetInt("mistake", 4);
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("ViljamiScene");
-            return;
-        }
-        if (PlayerPrefs.GetInt("mistake") == 4) {
-            SceneManager.LoadScene("ViljamiScene");
-            return;
-        }
-    }
     public void ChaosMode() {
         chaos = true;
     }
--- ThrowTheBottle/Assets/Scripts/Test Scripts/Secret.cs
+++ ThrowTheBottle/Assets/Scripts/Test Scripts/Secret.cs
@@ -6,8 +6,37 @@ using UnityEngine.SceneManagement;
 public class Secret : MonoBehaviour
 {
     int clicks = 0;
+    public bool nightmare = false;
+
+    private void Start() {
+        if (nightmare) {
+            if (PlayerPrefs.GetInt("mistake") == 1) {
+                PlayerPrefs.SetInt("mistake", 2);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene("ViljamiScene");
+                return;
+            }
+            if (PlayerPrefs.GetInt("mistake") == 2) {
+                PlayerPrefs.SetInt("mistake", 3);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene("ViljamiScene");
+                return;
+            }
+            if (PlayerPrefs.GetInt("mistake") == 3) {
+                PlayerPrefs.SetInt("mistake", 4);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene("ViljamiScene");
+                return;
+            }
+            if (PlayerPrefs.GetInt("mistake") == 4) {
+                SceneManager.LoadScene("ViljamiScene");
+                return;
+            }
+        }
+    }
     public void Click() {
         clicks++;
+        print("click " + clicks);
         if (clicks >= 12) {
             SceneManager.LoadScene("ViljamiScene");
         }
